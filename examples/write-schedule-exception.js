9f8e2e227c016ee5e71eff850fa663427def3760
Even Gunnes <even@evolo.no>
2023-06-14 13:45:27 +0200

    up version number and enable effective period to parse raw calendar data

--- examples/write-schedule-exception.js
+++ examples/write-schedule-exception.js
@@ -16,7 +16,6 @@ bacnetClient.writeProperty(
   { type: 17, instance: 1 },
   Bacnet.enum.PropertyIdentifier.EXCEPTION_SCHEDULE,
   [
-    // one row for each exception
     {
       date: {
         type: 10,
@@ -57,8 +56,81 @@ bacnetClient.writeProperty(
     },
     {
       date: {
+        type: 102,
+        value: [
+          {
+            type: 10,
+            value: 1672531200000,
+          },
+          {
+            type: 10,
+            value: 1675209600000,
+          },
+        ],
+      },
+      events: [
+        {
+          time: {
+            type: 11,
+            value: -2174716800000,
+          },
+          value: {
+            type: 9,
+            value: 3,
+          },
+        },
+      ],
+      priority: {
+        type: 3,
+        value: 2,
+      },
+    },
+    {
+      date: {
         type: 10,
-        value: 1685224800000,
+        value: 1684965600000,
+        raw: {
+          year: 120,
+          month: 3,
+          day: 4,
+          wday: 255,
+        },
+      },
+      events: [
+        {
+          time: {
+            type: 11,
+            value: -2174778000000,
+          },
+          value: {
+            type: 1,
+            value: true,
+          },
+        },
+        {
+          time: {
+            type: 11,
+            value: -2174774400000,
+          },
+          value: {
+            type: 1,
+            value: false,
+          },
+        },
+      ],
+      priority: {
+        type: 3,
+        value: 1,
+      },
+    },
+    {
+      date: {
+        type: 101,
+        value: {
+          month: 1,
+          week: 1,
+          wday: 2,
+        },
       },
       events: [
         {
--- examples/write-schedule-period.js
+++ examples/write-schedule-period.js
@@ -17,8 +17,8 @@ bacnetClient.writeProperty(
   Bacnet.enum.PropertyIdentifier.EFFECTIVE_PERIOD,
   [
     // write -3600000 to set 'any'
-    { type: 10, value: 1714083064000 },
-    { type: 10, value: -3600000 },
+    { type: 10, value: 1714083064000, raw: { day: 1, month: 1, year: 120, wday: 3 } },
+    { type: 10, value: -3600000, raw: { day: 1, month: 1, year: 122, wday: 6 } },
   ],
   (err, value) => {
     if (err) console.log(err);
