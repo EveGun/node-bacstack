7461c55627a9e51c95d3cc462297e162ec68a37b
Even Gunnes <even@evolo.no>
2024-02-21 09:10:18 +0100

    wip respond to discovery

--- examples/read-device-object-list.js
+++ examples/read-device-object-list.js
@@ -16,6 +16,6 @@ bacnetClient.on('error', (err) => {
   if (value) console.log(value);
   //bacnetClient.close();
 });*/
-bacnetClient.readProperty('192.168.40.50', { type: 8, instance: 620643 }, Bacnet.enum.PropertyIdentifier.OBJECT_LIST, { maxSegments: Bacnet.enum.MaxSegmentsAccepted.SEGMENTS_65 }, (err, value) => {
+bacnetClient.readProperty('192.168.40.50', { type: 8, instance: 620643 }, Bacnet.enum.PropertyIdentifier.OBJECT_LIST, { maxSegments: Bacnet.enum.MaxSegmentsAccepted.SEGMENTS_0 }, (err, value) => {
   console.log(err, value);
 });
