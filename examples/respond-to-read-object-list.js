6565e28d1a8445f6d539dac6ff22d03a5055c079
Even Gunnes <even@evolo.no>
2024-02-21 09:10:23 +0100

    wip respond to discovery

---
+++ examples/respond-to-read-object-list.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Bacnet = require('../index');
+
+// create instance of Bacnet
+const bacnetClient = new Bacnet({ apduTimeout: 2000 });
+
+// emitted on errors
+bacnetClient.on('error', (err) => {
+  console.error(err);
+  bacnetClient.close();
+});
+
+/*bacnetClient.readProperty('192.168.40.50', { type: 8, instance: 620643 }, Bacnet.enum.PropertyIdentifier.OBJECT_LIST, { maxSegments: Bacnet.enum.MaxSegmentsAccepted.SEGMENTS_0 }, (err, value) => {
+  if (err) console.log(err);
+  if (value) console.log(value);
+  //bacnetClient.close();
+});*/
+bacnetClient.readProperty('192.168.40.245', { type: 8, instance: 10000 }, Bacnet.enum.PropertyIdentifier.OBJECT_LIST, { maxSegments: Bacnet.enum.MaxSegmentsAccepted.SEGMENTS_0 }, (err, value) => {
+  console.log(err, value);
+});
