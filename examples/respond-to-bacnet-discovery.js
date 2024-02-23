c155b3f2258f19d54eb70eeafa25a89ee7d18d85
Even Gunnes <even@evolo.no>
2024-02-23 10:41:33 +0100

    finished bacnet respond to whoIs and support readPropertyMultiple and readProperty

---
+++ examples/respond-to-bacnet-discovery.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Bacnet = require('../index');
+
+// create instance of Bacnet
+const bacnetClient = new Bacnet({ apduTimeout: 2000, port: 47808, bacnetDeviceOptions: { deviceInstance: 10000 } });
+
+// emitted on errors
+bacnetClient.on('error', (err) => {
+  console.error(err);
+  bacnetClient.close();
+});
+
+bacnetClient.on('whoIs', (msg) => {
+  if (!Object.keys(msg.payload).length || (msg.payload.lowLimit <= 10000 && msg.payload.highLimit >= 10000)) {
+    bacnetClient.iAmResponse(msg.header.sender.address, 10000, Bacnet.enum.Segmentation.NO_SEGMENTATION, 0, (err, value) => {});
+    console.log('responded to who is');
+  }
+});
